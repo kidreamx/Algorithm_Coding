c5d2967305fea49036bc22b7c5e933915fe3dcdf
윤경일 <105635950+kidreamx@users.noreply.github.com>
2025-01-18 23:49:35 +0900

    [Bronze V] Title: 카드 게임, Time: 100 ms, Memory: 14220 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int result=0;
+        for(int i=0;i<5;i++){
+            result += Integer.parseInt(br.readLine());
+        }
+        bw.write(result+"");
+        bw.flush();
+        bw.close();
+    }
+
+}
