f48ee044d14a8f6a094aee04b809fc644451d8c9
윤경일 <105635950+kidreamx@users.noreply.github.com>
2025-02-01 01:00:46 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 776 ms, Memory: 122204 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -6,26 +6,25 @@ public class Main{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] chars = br.readLine().toCharArray();
         int N =Integer.parseInt(br.readLine());
-        int[][] alpha = new int[chars.length][26];
-        for(int i=0;i<chars.length;i++){
-            if(i==0) alpha[i][chars[i]-'a']++;
-            else alpha[i][chars[i]-'a'] = alpha[i-1][chars[i]-'a']+ 1;
+        StringBuilder sb = new StringBuilder();
+        int[][] alpha = new int[chars.length+1][26];
+        for(int i=1;i<=chars.length;i++){
             for(int j=0;j<26;j++){
-                if(i==chars.length-1) continue;
-                alpha[i+1][j] = alpha[i][j];
+                if(j==chars[i-1]-'a') alpha[i][j] = alpha[i-1][j]+1;
+                else alpha[i][j] = alpha[i-1][j];
             }
         }
         int[] answer = new int[N];
         for(int i=0;i<N;i++){
             String[] sa = br.readLine().split(" ");
             char c = sa[0].charAt(0);
-            int start = Integer.parseInt(sa[1]);
-            int end = Integer.parseInt(sa[2]);
-            if(start==0) answer[i] = alpha[end][c-'a'];
-            else answer[i] = alpha[end][c-'a'] - alpha[start-1][c-'a'];
+            int start = Integer.parseInt(sa[1])+1;
+            int end = Integer.parseInt(sa[2])+1;
+            answer[i] = alpha[end][c-'a'] - alpha[start-1][c-'a'];
         }
         for(int i=0;i<N;i++){
-            System.out.println(answer[i]);
+            sb.append(answer[i]+"\n");
         }
+        System.out.println(sb);
     }
 }
