c060b51a1f8371cb28b3066c409ee85ac2c505be
윤경일 <105635950+kidreamx@users.noreply.github.com>
2025-02-20 21:46:08 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 1108 ms, Memory: 153496 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.java
@@ -1,46 +1,39 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
-
-public class Main {
-
-    public static void main(String[] args) throws IOException {
+public class Main{
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
         String[] sa = br.readLine().split(" ");
         int N = Integer.parseInt(sa[0]);
         int M = Integer.parseInt(sa[1]);
+        int[][] dp = new int[N+1][N+1];
         int[][] arr = new int[N+1][N+1];
-        int[][] line = new int[N+1][N+1];
-        int sum=0;
-        for(int i = 1; i <= N; i++){
-            sa = br.readLine().split(" ");
-            for(int j = 1; j <= N; j++){
+        for(int i=1;i<=N;i++){
+            sa= br.readLine().split(" ");
+            for(int j=1;j<=N;j++){
                 arr[i][j] = Integer.parseInt(sa[j-1]);
-                line[i][j] = arr[i][j] + line[i-1][j];
+                dp[i][j] = arr[i][j]+ dp[i-1][j];
             }
         }
-        for(int i = 1; i <= M; i++){
+        for(int i=0;i<M;i++){
+            int sum=0;
             sa = br.readLine().split(" ");
-            int start_x = Integer.parseInt(sa[0]);
-            int start_y = Integer.parseInt(sa[1]);
-            int end_x = Integer.parseInt(sa[2]);
-            int end_y = Integer.parseInt(sa[3]);
-            for(int j = start_y; j <= end_y; j++){
-                if(start_x == 1){
-                    sum+= line[end_x][j];
+            int x1 = Integer.parseInt(sa[0]);
+            int y1 = Integer.parseInt(sa[1]);
+            int x2 = Integer.parseInt(sa[2]);
+            int y2 = Integer.parseInt(sa[3]);
+            for(int j= y1 ; j<= y2;j++){
+                if(x1==1){
+                    sum+= dp[x2][j];
                 }
                 else{
-                    sum+= line[end_x][j] - line[start_x-1][j];
+                    sum += dp[x2][j]- dp[x1-1][j];
                 }
             }
-            bw.write(sum+"\n");
-            sum=0;
+            sb.append(sum+"\n");
         }
-        bw.flush();
-        bw.close();
-
+        System.out.println(sb);
     }
-
 }
-
