cfe9641d41fd241ec1da26cb874cd9d027796e75
윤경일 <105635950+kidreamx@users.noreply.github.com>
2025-03-01 17:53:43 +0900

    [Gold III] Title: 앱, Time: 124 ms, Memory: 14568 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -1,38 +1,47 @@
 import java.util.*;
 import java.io.*;
 
-public class Main{
-    public static void main(String[] args) throws IOException{
+public class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] sa = br.readLine().split(" ");
-        int N = Integer.parseInt(sa[0]);
-        int M = Integer.parseInt(sa[1]);
-        int[] memory = new int[N+1];
-        sa = br.readLine().split(" ");
-        for(int i=1;i<=N;i++){
-            memory[i] = Integer.parseInt(sa[i-1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        int[] memory = new int[n + 1];
+        int[] cost = new int[n + 1];
+        
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) {
+            memory[i] = Integer.parseInt(st.nextToken());
         }
-        int[] cost = new int[N+1];
-        int costSum = 0;
-        sa = br.readLine().split(" ");
-        for(int i=1;i<=N;i++){
-            cost[i]  = Integer.parseInt(sa[i-1]);
-            costSum += cost[i];
+        
+        st = new StringTokenizer(br.readLine());
+        int sumCost = 0;
+        for (int i = 1; i <= n; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+            sumCost += cost[i];
         }
-        int[] dp = new int[costSum+1];
-        for(int i=1;i<=N;i++){
-            for(int j=costSum;j>=cost[i];j--){
-                dp[j] = Math.max(dp[j],dp[j-cost[i]]+memory[i]);
+        
+        // dp[c] : 비용의 합이 c일 때 해제할 수 있는 최대 메모리
+        int[] dp = new int[sumCost + 1];
+        // dp[0] = 0으로 초기화되었으므로 이미 초기값 세팅 완료
+        
+        // 0-1 배낭 문제 방식으로 업데이트 (역순 순회)
+        for (int i = 1; i <= n; i++) {
+            for (int j = sumCost; j >= cost[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - cost[i]] + memory[i]);
             }
         }
+        
+        // dp[c]가 m 이상이 되는 최소 c를 찾음
         int answer = Integer.MAX_VALUE;
-        for(int i=1;i<=costSum;i++){
-            if(dp[i]>=M){
-                answer = i;
+        for (int c = 0; c <= sumCost; c++) {
+            if (dp[c] >= m) {
+                answer = c;
                 break;
             }
         }
-        if(answer == Integer.MAX_VALUE) System.out.println(0);
-        else System.out.println(answer);
+        System.out.println(answer);
     }
 }
