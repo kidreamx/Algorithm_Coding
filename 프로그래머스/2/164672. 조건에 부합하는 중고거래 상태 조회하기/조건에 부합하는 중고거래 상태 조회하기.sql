bd9c3e44f56261c68a10e98d4dd51bf0ec34e206
윤경일 <kidreamx@gmail.com>
2026-06-04 23:17:57 +0900

    [level 2] Title: 조건에 부합하는 중고거래 상태 조회하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/164672. 조건에 부합하는 중고거래 상태 조회하기/조건에 부합하는 중고거래 상태 조회하기.sql
@@ -0,0 +1,10 @@
+-- 코드를 입력하세요
+SELECT BOARD_ID , WRITER_ID , TITLE , PRICE , 
+case 
+    WHEN STATUS = 'SALE' then '판매중'
+    WHEN STATUS = 'RESERVED' then '예약중'
+    WHEN STATUS = 'DONE' then '거래완료'
+end as STATUS
+FROM USED_GOODS_BOARD 
+WHERE DATE(CREATED_DATE) = '2022-10-05'
+ORDER BY BOARD_ID DESC;
