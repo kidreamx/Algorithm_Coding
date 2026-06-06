317b26ec206c8a8ce1448b6f8ac40cc161fa8b59
윤경일 <kidreamx@gmail.com>
2026-06-06 22:14:13 +0900

    [level 3] Title: 가장 먼 노드, Time: 12.55 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,47 +1,36 @@
 import java.util.*;
-class Edge{
-    int dot , len;
-    Edge(int dot , int len){
-        this.dot = dot;
-        this.len = len;
-    }
-}
 class Solution {
     public int solution(int n, int[][] edge) {
+        int answer = 0;
         List<List<Integer>> list = new ArrayList<>();
-        int len = edge.length;
+        int[] dist = new int[n+1];
+        Arrays.fill(dist,-1);
         for(int i=0;i<=n;i++){
             list.add(new ArrayList<>());
         }
-        for(int i=0;i<len;i++){
+        for(int i=0;i<edge.length;i++){
             list.get(edge[i][0]).add(edge[i][1]);
             list.get(edge[i][1]).add(edge[i][0]);
         }
-        int result=0;
-        boolean[] visited = new boolean[n+1];
-        Queue<Edge>queue = new ArrayDeque<>();
-        queue.add(new Edge(1,0));
-        int[] cnt = new int[n+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(1);
+        dist[1] = 0;
         while(!queue.isEmpty()){
-            Edge e = queue.poll();
-            if(visited[e.dot]) continue;
-            visited[e.dot] = true;
-            cnt[e.dot] = e.len;
-            for(int i : list.get(e.dot)){
-                if(visited[i]) continue;
-                queue.add(new Edge(i,e.len+1));
+            int num = queue.poll();
+            for(int next : list.get(num)){
+                if(dist[next]==-1){
+                    dist[next] = dist[num]+1;
+                    queue.add(next);
+                }
             }
         }
-        int max =0;
+        int max=0;
         for(int i=1;i<=n;i++){
-            max = Math.max(cnt[i],max);
+            max = Math.max(max, dist[i]);
         }
         for(int i=1;i<=n;i++){
-            if(max == cnt[i]){
-                result++;
-            }   
+            if(dist[i]==max) answer++;
         }
-        return result;
+        return answer;
     }
-    
 }
